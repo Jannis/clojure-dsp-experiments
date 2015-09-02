ad61a9d911d2ea7d72e987af01abcfb9419ae680
Jannis Pohlmann <jannis@xfce.org>
2015-09-02 14:16:21 +0200

    dsp.comp, dsp.core: Add a very stupid compressor; tbc

--- src/dsp/comp.clj
+++ src/dsp/comp.clj
@@ -3,16 +3,63 @@
 
 
 
-(defn limit-sample
+(defn- limit-sample
   "Limits a sample by a given dB value."
-  [x db]
-  (let [limit (db2amp db)]
+  [x threshold]
+  (let [threshold (db2amp threshold)]
     (if (< x 0)
-      (max x (* limit -1))
-      (min x limit))))
+      (max x (* threshold -1))
+      (min x threshold))))
 
 
 (defn stupid-limiter
   "A very harsh and stupid limiter."
-  [samples db]
-  (map #(limit-sample %1 db) samples))
+  [samples threshold]
+  (map #(limit-sample %1 threshold) samples))
+
+
+(defn apply-ratio
+  [x threshold ratio]
+  (let [threshold (db2amp threshold)
+        sign      (java.lang.Math/signum x)
+        over      (- (java.lang.Math/abs x) threshold)]
+    (if (> over 0)
+      (* sign (+ threshold (* over (/ 1.0 ratio))))
+      x)))
+
+
+(defn stupid-level
+  [amplitudes]
+  (map #(java.lang.Math/abs %1) amplitudes))
+
+
+(defn stupid-overshoot
+  [levels threshold]
+  (map #(max (- %1 (db2amp threshold)) 0) levels))
+
+
+(defn stupid-gain-reduction
+  [overshoot ratio]
+  (map #(/ %1 ratio) overshoot))
+
+
+(defn apply-sample-gain-reduction
+  [x o r]
+  (* (java.lang.Math/signum x)
+     (+ (- (java.lang.Math/abs x) o) r)))
+
+
+(defn apply-gain-reduction
+  [amplitudes overshoot reduction]
+  (map apply-sample-gain-reduction amplitudes overshoot reduction))
+  ; (map #(- %1 (* (java.lang.Math/signum %1) %2))
+      ;  amplitudes reduction))
+
+
+(defn stupid-compressor
+  "A stupid compressor with only threshold and ratio parameters."
+  [samples threshold ratio]
+  (let [level          (stupid-level samples)
+        overshoot      (stupid-overshoot level threshold)
+        gain-reduction (stupid-gain-reduction overshoot ratio)]
+    (apply-gain-reduction samples overshoot gain-reduction)))
--- src/dsp/core.clj
+++ src/dsp/core.clj
@@ -10,8 +10,11 @@
 
 (defn -main
   [& args]
-  (doseq [samples (comp-test-sines 100 5512)]
-    (let [times (sample-times samples 5512)
-          limited (stupid-limiter samples -6.0)]
+  (doseq [samples (comp-test-sines 10 1000)]
+    (let [times      (sample-times samples 1000)
+          ; limited    (stupid-limiter samples -6.0)
+          compressed (stupid-compressor samples -6.0 2.0)]
       (incanter/view (-> (charts/xy-plot times samples)
-                         (charts/add-lines times limited))))))
+                        ;  (charts/add-lines times limited)
+                         (charts/add-lines times compressed))
+                     :width 1200 :height 600))))
