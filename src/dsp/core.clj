b9f45509b9d211d80027449db8fa95d297a80256
Jannis Pohlmann <jannis@xfce.org>
2015-09-03 14:40:05 +0200

    dsp.core: Render hard and soft knee compression side by side

--- src/dsp/core.clj
+++ src/dsp/core.clj
@@ -34,7 +34,11 @@
             compressed1 (soft-knee-compressor samples
                                               :threshold -6.0
                                               :ratio      6.0
-                                              :knee       0.0)]
+                                              :knee       0.0)
+            compressed2 (soft-knee-compressor samples
+                                              :threshold -6.0
+                                              :ratio      6.0
+                                              :knee      12.0)]
         (incanter/view (-> (charts/xy-plot times samples
                                            :title "Compressed Test Signal (Threshold: -6dB)"
                                            :x-label "Time"
@@ -42,28 +46,7 @@
                                            :legend true
                                            :series-label "Original Signal")
                             (charts/add-lines times compressed1
-                                              :series-label "Ratio 6:1, Knee 0dB"))
+                                              :series-label "Ratio 6:1, Knee 0dB")
+                            (charts/add-lines times compressed2
+                                              :series-label "Ratio 6:1, Knee 12dB"))
                        :width 1000 :height 600)))))
-    ; (doseq [samples (comp-test-signals 10 1000)]
-    ;   (let [times       (sample-times samples 1000)
-    ;         ; limited    (stupid-limiter samples -6.0)
-    ;         compressed1 (stupid-compressor samples
-    ;                                        :threshold -6.0
-    ;                                        :ratio 10.0
-    ;                                        :makeup-gain 0.0)
-    ;         compressed2 (soft-knee-compressor samples
-    ;                                           :threshold -6.0
-    ;                                           :ratio 10.0
-    ;                                           :makeup-gain 0.0
-    ;                                           :knee 6.0)
-    ;         ]
-    ;     ; (incanter/view (-> (charts/xy-plot in1 out1)
-    ;     ;                    (charts/add-lines in2 out2)
-    ;     ;                   ;  (charts/add-function identity -64 0 :step-size 0.064)
-    ;     ;                    (charts/add-lines [-24 -24] [-64 0]))
-    ;     ;                :width 600 :height 600))))
-    ;     (incanter/view (-> (charts/xy-plot times samples))
-    ;                        (charts/add-lines times compressed1)
-    ;                        (charts/add-lines times compressed2)
-    ;                    :width 1200 :height 600)
-    ;                       ;  (charts/add-lines times limited)
